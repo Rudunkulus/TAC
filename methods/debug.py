6db13e92053b6202e4eb369e242eae20de363695
Ruben Leidel <rudunk-busi@outlook.de>
2025-01-03 17:11:52 +0100

    added possibility of clicking center

--- methods/debug.py
+++ methods/debug.py
@@ -2,7 +2,10 @@ from methods import draw, actions
 from classes import DATA
 
 def mouseClick(data:DATA.Data, win, clock, square:int):
-    x, y = data.board.squaresXY[square]
+    if square == -1: # center
+        x, y = data.constants.xCenter, data.constants.yCenter
+    else:
+        x, y = data.board.squaresXY[square]
     actions.mouseClick(data, x, y)
     draw.waitForAnimation(data, win, clock)
 
